c046f4946c486313b8d1d6e99f63110fc47c1140
EvHafling <evan.hafling@students.first.edu>
2022-09-19 18:16:44 -0400

    c

--- RPS/assets/js/logic.js
+++ RPS/assets/js/logic.js
@@ -21,3 +21,33 @@ function getRandomPick () {
       }
 
 }
+
+Function getResultText(computerPick, playerPick) {
+  const TIE = "It's a tie!"; 
+  const WIN = "You win!"; 
+  const LOSE = "You lose!";
+
+  if (computerPick == playerPick) {
+   return TIE;
+  }
+
+  if (computerPick == PAPER && playerPick == ROCK) {
+  return LOSE; } 
+    else if (computerPick == PAPER && playerPick == SCISSORS) {
+  return WIN;
+    }
+
+
+  if (computerPick == ROCK && playerPick == SCISSORS) {
+  return LOSE; } 
+    else if (computerPick == ROCK && playerPick == PAPER) {
+  return WIN;
+    }
+
+  if (computerPick == SCISSORS && playerPick == PAPER) {
+  return LOSE; } 
+  else if (computerPick == SCISSORS && playerPick == ROCK) {
+  return WIN;
+  }
+
+}
