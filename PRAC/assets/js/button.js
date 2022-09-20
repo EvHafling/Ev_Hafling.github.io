6df08f273b54486d6a7c3dd36cb28be052a37939
EvHafling <evan.hafling@students.first.edu>
2022-09-19 20:55:42 -0400

    c

--- PRAC/assets/js/button.js
+++ PRAC/assets/js/button.js
@@ -1,53 +1,10 @@
+const btn = document.querySelector('button');
 
-
-const ROCK = "ROCK";
-const PAPER = "PAPER";
-const SCISSORS = "SCISSORS";
-
-
-function getRandomPick () {
-
-    let chance = Math.random();
-
-    if (chance > 0.66) {
-        return ROCK;
-
-    } else if (chance > 0.33) {
-
-        return PAPER;
-    }
-      else {
-        return SCISSORS;
-      }
-
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
 }
 
-function getResultText(computerPick, playerPick) {
-  const TIE = "It's a tie!"; 
-  const WIN = "You win!"; 
-  const LOSE = "You lose!";
-
-  if (computerPick == playerPick) {
-   return TIE;
-  }
-
-  if (computerPick == PAPER && playerPick == ROCK) {
-  return LOSE; } 
-    else if (computerPick == PAPER && playerPick == SCISSORS) {
-  return WIN;
-    }
-
-
-  if (computerPick == ROCK && playerPick == SCISSORS) {
-  return LOSE; } 
-    else if (computerPick == ROCK && playerPick == PAPER) {
-  return WIN;
-    }
-
-  if (computerPick == SCISSORS && playerPick == PAPER) {
-  return LOSE; } 
-  else if (computerPick == SCISSORS && playerPick == ROCK) {
-  return WIN;
-  }
-
-}
+btn.addEventListener('click', function () {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+});
